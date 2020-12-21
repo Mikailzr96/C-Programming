41529a36498ae5acb7c28059d454e23a440cba65
Mikailzr96 <mikail.ozturk41@hotmail.com>
2020-12-21 21:00:07 +0300

    Pointers

---
+++ Pointers Example 3 Find Arrey Max - Min Value/Pointers Example 3 Find Arrey Max - Min Value.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+#define		SIZE		10
+
+int find_max_min_value(int* a, int length, int* max, int* min)
+{
+	*max = *min = *a;
+
+	for (int i = 1; i < length; i++)
+	{
+		if (a[i] > *max)
+			*max = a[i];
+		else if (a[i] < *min)
+			*min = a[i];
+	}
+}
+int main() 
+{
+	setlocale(LC_ALL, "Turkish");
+	
+	int max, min;
+	int a[SIZE] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	find_max_min_value(a, SIZE, &max, &min);
+	printf("Max Value --> %d \nMin Value --> %d \n",max,min);
+
+}
---
+++ Pointers Example 3/Pointers Example 3.c
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+#define		SIZE		10
+
+int arrey_max_value(const int* p1, int len);
+int arrey_max_valeu_with_pointer(const int* p2, int len);
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+
+	int a[SIZE] = { 125,19,3800,1,-56,2758,10000,89,933,0 };
+
+	int* ptr1 = arrey_max_valeu_with_pointer(a, SIZE);
+	
+	printf("Max Value --> %d \n", arrey_max_value(a, SIZE));
+	printf("Max Value --> %d \n", *ptr1);
+
+}
+
+int arrey_max_value(const int* p1, int len) {
+
+	int max = *p1;		// max = *p --> *p = a[0];
+
+	for (int i = 1; i < len; ++i) {
+		if (p1[i] > max) {
+			max = p1[i];
+		}
+	}
+	return max;
+}
+
+int arrey_max_valeu_with_pointer(const int* p3, int len) {
+	int* ptrmax = (int*)p3;
+
+	for (int i = 0; i < len; i++) {
+		if (p3[i] > *ptrmax)
+			ptrmax = (int*)p3 + i;
+	}
+	return ptrmax;
+}
