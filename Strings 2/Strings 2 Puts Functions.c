7e3f5e1919589c4ae5ef0beef0b5260120bead3f
Mikailzr96 <mikail.ozturk41@hotmail.com>
2021-01-19 09:01:02 +0300

    Basic C

---
+++ Strings 2/Strings 2 Puts Functions.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+void puts1(const char* s);
+void puts2(const char* s);
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+
+	puts1("Hello");
+	puts2("World");
+}
+
+void puts1(const char* s) {
+
+	while (*s != '\0' )
+		putchar(*s++);
+	putchar('\n');
+}
+void puts2(const char* s) {
+
+	for (int i = 0; s[i] != '\0'; i++)
+		putchar(s[i]);
+	putchar('\n');
+}
---
+++ Strings 3 Gets Functions/Strings 3 Gets Functions.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+void string_gets_s(char* s, int n);
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+
+	char s2[10];
+	string_gets_s(s2, 10);
+}
+
+void string_gets_s(char* s, int n) {
+	int c;
+	int i = 0;
+	while ((c = getchar() != '\n'))
+		if (i < n)
+			s[i++] = c;
+	s[i] = '\0';
+}
+
+
