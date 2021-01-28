07079ea46e328d56e5d5b6d1844c040738d6cc0b
Mikailzr96 <mikail.ozturk41@hotmail.com>
2021-01-28 16:35:20 +0300

    Basic C

---
+++ Unions 1/Unions 1.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+	Birlikler de yapılar gibi aynı yöntemle kullanılır ve çalışırlar. Fakat tek fark ise burada bütün değişekenler hafızada aynı noktada 
+	buluşur. Grekeli hafıza sadece en büyük değişken için ayrılmıştır. Diğer tüm değişkenler aynı hafıza yerini paylaşmak zorundadır.
+	Herhangi bir zamanda üyelerinden en fazla bir tanesinin bir değer tutacağını bildiğimiz zamanlarda, bellekten tasarruf etmek 
+	amacıyla genelde birlikler kullanılır.
+*/
+struct yap {
+	int id1;
+	int id2;
+	int id3;
+	double dd;
+	char cd1;
+	char cd2;
+	char cd3;
+} yd;
+union bir {
+	int id1;
+	int id2;
+	int id3;
+	double dd;
+	char cd1;
+	char cd2;
+	char cd3;
+} bd;
+
+int main(void)
+{
+	printf("Yapı boyutu: %d \n", sizeof(yd));
+	printf("Bileşim boyutu: %d \n", sizeof(bd));
+
+/*
+	Aynı elemanlara sahip olmalarına rağmen yapıya eklenen elemanların yapı boyutunu artırdığı, 
+	bileşim elemanlarının ise sadece en büyük olanının bileşim boyutunu belirlediği görülür.
+*/
+
+}
---
+++ Unions 2/Unions 2.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+union bir {
+	int id;
+	double dd;
+	char cd;
+} bd;
+
+int main()
+{
+	bd.id = 21;
+	bd.cd = 'A';
+
+	printf("%d %c \n", bd.id, bd.cd); /* 1 */
+
+	bd.id = 127;
+	printf("%d %c \n", bd.id, bd.cd); /* 2 */
+
+	bd.dd = 34.75;
+	printf("%d %.2f \n", bd.id, bd.dd);   /* 3 */
+
+	/*
+	Program, 1 sayısı ile gösterilen işlem satırında bir bileşiminde yer alan cd değişkeninin, 
+	2 sayısı ile gösterilen işlem satırında id değişkeninin ve 
+	3 sayısı ile gösterilen işlem satırında dd değişkeninin değerini normal olarak ekrana yazar.
+	Aynı işlem satırlarında bahsi geçen değişkenler dışında kalan değerleri istendiği şekilde ekrana yazmaz,
+	çünkü aynı bileşim içinde yer alan değişkenlerden aynı anda sadece bir tanesi ayrılan bellek bölgesini kullanabilir.
+	Bu nedenle, sadece en son olarak atama yapılan bileşim değişkeni ekrana normal olarak yazılabilir.
+	*/
+
+
+}
